0b9cb729adf57c00badd78b9583024e0aa3449f8
Andy De Luna <raidenshogun05@gmail.com>
2024-11-08 17:22:49 -0600

    Added color and gradient

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,9 @@ function App() {
 
   const searchTracks = async (query) => {
     try {
+      if (track) {
+        setTrack('');
+      }
       const response = await fetch(
         `https://api.spotify.com/v1/search?q=${query}&type=track`,
         {
--- src/components/songpreview.jsx
+++ src/components/songpreview.jsx
@@ -2,9 +2,10 @@ export default function SongPreview(props) {
   const URL = `https://open.spotify.com/embed/track/${props.ID}`;
   return (
     <iframe
+      className="preview"
       src={URL}
-      width="500"
-      height="300"
+      width="600"
+      height="350"
       frameborder="0"
       allowtransparency="true"
       allow="encrypted-media"
